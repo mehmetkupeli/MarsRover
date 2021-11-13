cb5593d62428530dac8e36339a2855a73d1785e8
Mehmet Küpeli <57587797+mehmetkupeli@users.noreply.github.com>
2021-11-13 05:07:28 +0300

    Successed Rover All Tests

--- TestRover/UnitTest1.cs
+++ TestRover/UnitTest1.cs
@@ -36,5 +36,24 @@ namespace TestRover
             string testResult = testRover.ToString();
             Assert.Equal("1 5 N", testResult);
         }
+
+        [Fact]
+        public void RoverCheckOutputOne()
+        {
+            Ground testGround = new Ground(new Position(5, 5));
+            Rover testRover = new Rover(testGround, new Position(3, 3), Directions.E);
+            testRover.Process("MMRMMRMRRM");
+            string testResult = testRover.ToString();
+            Assert.Equal("5 1 E", testResult);
+        }
+        [Fact]
+        public void RoverCheckOutputTwo()
+        {
+            Ground testGround = new Ground(new Position(5, 5));
+            Rover testRover = new Rover(testGround, new Position(1, 2), Directions.N);
+            testRover.Process("LMLMLMLMM");
+            string testResult = testRover.ToString();
+            Assert.Equal("1 3 N", testResult);
+        }
     }
 }
